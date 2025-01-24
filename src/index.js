8089d407c0aebe4dab4862b67b17dca55c280b21
iam-vinnu <binaybehera101@gmail.com>
2025-01-24 20:30:08 +0530

    utils custom services

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
+import cookieParser from "cookie-parser";
+import cors from 'cors';
 
 dotenv.config({});
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+
+
+
 
 app.listen(PORT , ()=>{
     connectDB();
